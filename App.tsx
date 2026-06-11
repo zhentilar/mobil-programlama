d72cdc973d2e44e691dae869de08c9b3f7c2b381
mustafa <mgulmuscan@gmail.com>
2026-06-11 19:25:25 +0300

    Fix: JSON import hatası giderildi

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
   ScrollView,
   Alert,
 } from 'react-native';
-
+import kullaniciListesi from './kullanicilar.json';
 const { width } = Dimensions.get('window');
 
 // =====================
@@ -25,7 +25,7 @@ function GirisEkrani(props: any) {
     if (!eposta || !sifre) {
       Alert.alert('Hata', 'Lütfen e-posta ve şifre alanlarını doldurun');
       return;
-    } 
+    }
 
     const bulunanKullanici = kullaniciListesi.find(
       (user) => user.Eposta.toLowerCase() === eposta.toLowerCase() && user.Sifre === sifre
